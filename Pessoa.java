881e9b56efbcfc7e0aae7d38dfb34aa566859fdc
jujuhjuh <julianafonsecaribeirodasilva@gmail.com>
2025-11-21 16:17:45 -0300

    Create abstract class Pessoa with properties and methods

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+public abstract class Pessoa {
+    private String nome;
+    private String email;
+
+    public Pessoa(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void apresentar();
+}
